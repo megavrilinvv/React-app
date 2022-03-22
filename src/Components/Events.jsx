cb93bd115f4d86e954c920bdceaff12d75a0ad82
megavrilinvv <vastre@rambler.ru>
2022-03-22 15:11:17 +0300

    fix color

--- src/Components/Events.jsx
+++ src/Components/Events.jsx
@@ -4,7 +4,10 @@ const Events = () => {
   return (
     <>
       <h1>Введи PIN</h1>
-      <input onChange={(event) => console.log(event.target.value)} />
+      <input
+        className="in"
+        onChange={(event) => console.log(event.target.value)}
+      />
     </>
   );
 };
